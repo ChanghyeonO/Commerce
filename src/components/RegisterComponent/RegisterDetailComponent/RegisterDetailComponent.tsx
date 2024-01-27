5fdee34575fb86a92715b57e12e8531bb2533db6
ChanghyeonO <ckdgus5189@gmail.com>
2024-01-28 02:11:52 +0900

    feat: 파이어베이스 연동 완료, 회원가입 기능 테스트 완료

--- src/components/RegisterComponent/RegisterDetailComponent/RegisterDetailComponent.tsx
+++ src/components/RegisterComponent/RegisterDetailComponent/RegisterDetailComponent.tsx
@@ -1,5 +1,8 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { auth } from "../../../firebase";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+
 import FUNDITLOGO from "../../../assets/icons/FUNDIT.png";
 
 import {
@@ -32,6 +35,26 @@ import {
 } from "./RegisterDetailComponentStyle";
 
 const RegisterDetailComponent = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordCheck, setPasswordCheck] = useState("");
+  const [name, setName] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [address, setAddress] = useState("");
+  const [addressDetail, setAddressDetail] = useState("");
+  const navigate = useNavigate();
+
+  // const fullAddress = `${address} ${addressDetail}`;
+
+  const handleRegister = async () => {
+    try {
+      await createUserWithEmailAndPassword(auth, email, password);
+      navigate("/register-success");
+    } catch (error) {
+      console.error("회원가입 에러", error);
+    }
+  };
+
   return (
     <Container>
       <InnerContainer>
@@ -44,14 +67,39 @@ const RegisterDetailComponent = () => {
         <InnerContent>
           <InputContainer>
             <EmailInputArea>
-              <EmailInput type="text" placeholder="이메일" />
+              <EmailInput
+                type="text"
+                placeholder="이메일"
+                value={email}
+                onChange={e => setEmail(e.target.value)}
+              />
               <EmailCheckButton>인증요청</EmailCheckButton>
             </EmailInputArea>
-            <PasswordInput type="password" placeholder="패스워드" />
-            <PasswordCheckInput type="password" placeholder="패스워드 확인" />
-            <NameInput type="text" placeholder="이름" />
+            <PasswordInput
+              type="password"
+              placeholder="패스워드"
+              value={password}
+              onChange={e => setPassword(e.target.value)}
+            />
+            <PasswordCheckInput
+              type="password"
+              placeholder="패스워드 확인"
+              value={passwordCheck}
+              onChange={e => setPasswordCheck(e.target.value)}
+            />
+            <NameInput
+              type="text"
+              placeholder="이름"
+              value={name}
+              onChange={e => setName(e.target.value)}
+            />
             <PhoneNumberInputArea>
-              <PhoneNumberInput type="text" placeholder="전화번호" />
+              <PhoneNumberInput
+                type="text"
+                placeholder="전화번호"
+                value={phoneNumber}
+                onChange={e => setPhoneNumber(e.target.value)}
+              />
               <SendCertificationButton>인증요청</SendCertificationButton>
             </PhoneNumberInputArea>
             <CertificationCheckArea>
@@ -59,14 +107,22 @@ const RegisterDetailComponent = () => {
               <CertificationCheckButton>확인</CertificationCheckButton>
             </CertificationCheckArea>
             <FindAdressArea>
-              <FindAdressInput type="text" placeholder="주소" readOnly />
+              <FindAdressInput
+                type="text"
+                placeholder="주소"
+                value={address}
+                onChange={e => setAddress(e.target.value)}
+              />
               <FindAdressButton>주소검색</FindAdressButton>
             </FindAdressArea>
-            <FindAdressDetailInput type="text" placeholder="상세 주소" />
+            <FindAdressDetailInput
+              type="text"
+              placeholder="상세 주소"
+              value={addressDetail}
+              onChange={e => setAddressDetail(e.target.value)}
+            />
             <SuccessButtonArea>
-              <Link to="/register-success">
-                <SuccessButton>다음</SuccessButton>
-              </Link>
+              <SuccessButton onClick={handleRegister}>다음</SuccessButton>
             </SuccessButtonArea>
           </InputContainer>
         </InnerContent>
--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,8 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Header from "../../components/Header/Header";
 import LoginComponent from "../../components/LoginComponent/LoginCompontent";
+import { auth } from "../../firebase";
+import { createUserWithEmailAndPassword } from "firebase/auth";
 
 const Login = () => {
+  useEffect(() => {
+    createUserWithEmailAndPassword(auth, "test@test.com", "12341234");
+  }, []);
   return (
     <>
       <Header />
