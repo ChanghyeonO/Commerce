54ed43743d937ccd4181bae3f7de8bf05ac4327c
ChanghyeonO <ckdgus5189@gmail.com>
2024-02-18 10:30:49 +0900

    feat: 제품 추가하기 퍼블리싱 완료

--- src/components/ProductCreatorComponent/ProductCreatorComponent.tsx
+++ src/components/ProductCreatorComponent/ProductCreatorComponent.tsx
@@ -2,9 +2,12 @@ import React from "react";
 
 import {
   Container,
+  InnerContent,
+  Title,
+  IntroText,
   Header,
   ProductNameInput,
-  ProductDescriptionInput,
+  ProductDescriptionTextArea,
   OptionInputArea,
   OptionInput,
   OptionAddButton,
@@ -14,33 +17,46 @@ import {
   IntroContentArea,
   ImageArea,
   DescriptionImage,
-  TextArea,
   DescriptionText,
+  UploadButtonArea,
+  UploadButton,
 } from "./ProductCreatorComponentStyle";
 
 const ProductCreatorComponent = () => {
   return (
     <Container>
-      <Header>
-        <ProductNameInput />
-        <ProductDescriptionInput />
-        <OptionInputArea>
-          <OptionInput />
-          <OptionAddButton>추가</OptionAddButton>
-          <PriceAddInput />
-        </OptionInputArea>
-      </Header>
-      <Body>
-        <ContentAddButton>추가</ContentAddButton>
-        <IntroContentArea>
-          <ImageArea>
-            <DescriptionImage />
-          </ImageArea>
-          <TextArea>
-            <DescriptionText />
-          </TextArea>
-        </IntroContentArea>
-      </Body>
+      <InnerContent>
+        <Header>
+          <Title>제품 정보 작성하기</Title>
+          <ProductNameInput type="text" placeholder="제품명을 입력해주세요." />
+          <ProductDescriptionTextArea placeholder="제품에 대해 간단하게 소개해주세요" />
+          <OptionInputArea>
+            <IntroText>추가할 옵션을 작성해주세요</IntroText>
+            <OptionInput
+              type="text"
+              placeholder="추가할 옵션에 대해 작성해주세요"
+            />
+            <OptionAddButton>추가</OptionAddButton>
+            <PriceAddInput
+              type="number"
+              placeholder="판매 금액을 작성해주세요"
+            />
+          </OptionInputArea>
+        </Header>
+        <Body>
+          <IntroText>제품에 대한 추가 정보를 작성해주세요</IntroText>
+          <IntroContentArea>
+            <ImageArea>
+              <DescriptionImage />
+            </ImageArea>
+            <DescriptionText placeholder="제품에 대해 상세하게 설명해주세요" />
+          </IntroContentArea>
+          <ContentAddButton>추가</ContentAddButton>
+        </Body>
+        <UploadButtonArea>
+          <UploadButton>제품 추가하기</UploadButton>
+        </UploadButtonArea>
+      </InnerContent>
     </Container>
   );
 };
--- src/components/ProductCreatorComponent/ProductCreatorComponentStyle.ts
+++ src/components/ProductCreatorComponent/ProductCreatorComponentStyle.ts
@@ -5,30 +5,147 @@ export const Container = styled.div`
   min-height: 100vh;
 `;
 
-export const Header = styled.div``;
+export const InnerContent = styled.div`
+  width: 50%;
+  height: 100%;
+  margin: 0 auto;
+`;
 
-export const ProductNameInput = styled.input``;
+export const Title = styled.h1`
+  font-size: 30px;
+`;
 
-export const ProductDescriptionInput = styled.input``;
+export const IntroText = styled.h3`
+  font-size: 20px;
+`;
 
-export const OptionInputArea = styled.div``;
+export const Header = styled.div`
+  width: 100%;
+  height: 500px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+  gap: 10px;
+`;
 
-export const OptionInput = styled.input``;
+const InputDesign = styled.input`
+  width: 300px;
+  height: 40px;
+  padding: 0 10px;
+  border-radius: 12px;
+  border: none;
+  background-color: #eee;
+  font-size: 15px;
+  font-weight: bold;
+`;
 
-export const OptionAddButton = styled.button``;
+const TextAreaDesign = styled.textarea`
+  width: 100%;
+  height: 100px;
+  box-sizing: border-box;
+  background-color: #eee;
+  border: none;
+  overflow: scrolled;
+  outline: none;
+  resize: none;
+  border-radius: 12px;
+  padding: 10px;
+  font-size: 15px;
+  font-weight: bold;
+`;
 
-export const PriceAddInput = styled.input``;
+const ButtonDesign = styled.button`
+  width: 80px;
+  height: 40px;
+  border: none;
+  border-radius: 12px;
+  color: #fff;
+  font-size: 15px;
+  font-weight: bold;
+  cursor: pointer;
+  background-color: #38b6ff;
+`;
 
-export const Body = styled.div``;
+export const ProductNameInput = styled(InputDesign)``;
 
-export const ContentAddButton = styled.button``;
+export const ProductDescriptionTextArea = styled(TextAreaDesign)``;
 
-export const IntroContentArea = styled.div``;
+export const OptionInputArea = styled.div`
+  width: 100%;
+  min-height: 200px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+  gap: 10px;
+`;
 
-export const ImageArea = styled.div``;
+export const OptionInput = styled(InputDesign)``;
 
-export const DescriptionImage = styled.img``;
+export const OptionAddButton = styled(ButtonDesign)``;
+
+export const PriceAddInput = styled(InputDesign)`
+  &:focus {
+    outline: none;
+  }
+  &::-webkit-outer-spin-button,
+  &::-webkit-inner-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+  }
+  &[type="number"] {
+    -moz-appearance: textfield;
+  }
+`;
+
+export const Body = styled.div`
+  margin-bottom: 10px;
+`;
+
+export const ContentAddButton = styled(ButtonDesign)``;
+
+export const IntroContentArea = styled.div`
+  width: 100%;
+  min-height: 300px;
+  display: flex;
+  gap: 10px;
+  margin-bottom: 10px;
+`;
+
+export const ImageArea = styled.div`
+  width: 400px;
+  height: 300px;
+  background-color: #eee;
+  border-radius: 12px;
+`;
+
+export const DescriptionImage = styled.img`
+  width: 100%;
+  height: auto;
+`;
 
 export const TextArea = styled.div``;
 
-export const DescriptionText = styled.input``;
+export const DescriptionText = styled(TextAreaDesign)`
+  height: 300px;
+`;
+
+export const UploadButtonArea = styled.div`
+  width: 100%;
+  height: 40px;
+  display: flex;
+  justify-content: center;
+`;
+
+export const UploadButton = styled.button`
+  width: 220px;
+  height: 40px;
+  border-radius: 12px;
+  border: none;
+  background-color: #5271ff;
+  color: #fff;
+  font-size: 20px;
+  font-weight: bold;
+  cursor: pointer;
+`;
