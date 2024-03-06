509c80e248d1c580d7aee4452597e729527d2068
ChanghyeonO <ckdgus5189@gmail.com>
2024-03-06 18:42:34 +0900

    feat: 리액트 쿼리로 데이터 캐싱 처리

--- FrontEnd/src/components/ItemInfiniteScroll/ItemInfiniteScroll.tsx
+++ FrontEnd/src/components/ItemInfiniteScroll/ItemInfiniteScroll.tsx
@@ -1,14 +1,15 @@
-import { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
+import { useUser } from "../../contexts/UserContext";
+import { useInfiniteQuery } from "react-query";
 import { useInView } from "react-intersection-observer";
 import { fetchItems } from "../../api/api";
-import { db, storage, auth } from "../../api/firebase";
-import { doc, getDoc, deleteDoc } from "firebase/firestore";
+import { db, storage } from "../../api/firebase";
+import { doc, deleteDoc } from "firebase/firestore";
 import { ref as storageRef, deleteObject } from "firebase/storage";
 import Swal from "sweetalert2";
 import alertList from "../../utils/Swal";
 import { Item } from "../../types/ItemType";
-import { QueryDocumentSnapshot, DocumentData } from "firebase/firestore";
 
 import {
   Container,
@@ -21,38 +22,30 @@ import {
 import { DeleteButton } from "../ImageUpload/ImageUploadStyle";
 
 const ItemInfiniteScroll = () => {
-  const [items, setItems] = useState<Item[]>([]);
-  const [lastVisible, setLastVisible] =
-    useState<QueryDocumentSnapshot<DocumentData> | null>(null);
-  const [hasMore, setHasMore] = useState<boolean>(true);
-  const [isAdmin, setIsAdmin] = useState(false);
-
   const { ref, inView } = useInView();
   const navigate = useNavigate();
   const location = useLocation();
+  const { user } = useUser();
+  const isAdmin = user?.admin ?? false;
+
+  const collectionName = location.pathname.includes("/funding")
+    ? "fundingItems"
+    : "otherItems";
+
+  const fetchItemsWithCursor = async ({ pageParam = null }) => {
+    return fetchItems(collectionName, pageParam);
+  };
+
+  const { data, hasNextPage, fetchNextPage, isFetchingNextPage } =
+    useInfiniteQuery(["items", collectionName], fetchItemsWithCursor, {
+      getNextPageParam: (lastPage) => lastPage.lastVisible || undefined,
+    });
 
   useEffect(() => {
-    const checkAdminStatus = async () => {
-      const user = auth.currentUser;
-      if (user) {
-        const userRef = doc(db, "users", user.uid);
-        const docSnap = await getDoc(userRef);
-        if (docSnap.exists()) {
-          setIsAdmin(docSnap.data().admin);
-        }
-      }
-    };
-
-    checkAdminStatus();
-  }, []);
-
-  const getCollectionName = () => {
-    if (location.pathname.includes("/funding")) {
-      return "fundingItems";
-    } else if (location.pathname.includes("/other")) {
-      return "otherItems";
+    if (inView && hasNextPage) {
+      fetchNextPage();
     }
-  };
+  }, [inView, hasNextPage, fetchNextPage]);
 
   const handleItemClick = (id: string) => {
     const basePath = location.pathname.includes("/funding")
@@ -61,40 +54,10 @@ const ItemInfiniteScroll = () => {
     navigate(`${basePath}/detail/${id}`);
   };
 
-  const loadMoreItems = async () => {
-    if (!hasMore) return;
-    const collectionName = getCollectionName();
-    if (!collectionName) {
-      console.error("Collection name is undefined.");
-      return;
-    }
-    const {
-      items: newItems,
-      lastVisible: newLastVisible,
-      hasMore: newHasMore,
-    } = await fetchItems(collectionName, lastVisible);
-
-    setItems((prevItems) => [...prevItems, ...newItems]);
-    setLastVisible(newLastVisible);
-    setHasMore(newHasMore);
-  };
-
-  useEffect(() => {
-    if (inView && hasMore) {
-      loadMoreItems();
-    }
-  }, [inView, hasMore, location.pathname]);
-
   const deleteItemWithImage = async (
     itemId: string,
     itemDescriptions: { description: string; imageUrl: string }[],
   ) => {
-    const collectionName = getCollectionName();
-    if (!collectionName) {
-      console.error("컬렉션 이름을 찾을 수 없습니다.");
-      return;
-    }
-
     const result = await Swal.fire(
       alertList.doubleCheckMessage("정말로 삭제하시겠습니까?"),
     );
@@ -103,62 +66,53 @@ const ItemInfiniteScroll = () => {
       try {
         await deleteDoc(doc(db, collectionName, itemId));
 
-        const deletePromises = itemDescriptions.map((itemDescription) => {
-          const imageUrl = itemDescription.imageUrl;
+        const deletePromises = itemDescriptions.map(({ imageUrl }) => {
           const urlPath = new URL(imageUrl).pathname;
           const decodedPath = decodeURIComponent(urlPath)
             .split("/o/")[1]
             ?.split("?")[0];
-          if (!decodedPath) {
-            console.error("경로 추출 실패:", imageUrl);
-            return Promise.resolve();
-          }
-          const storagePath = `images/${decodedPath.replace(
-            /^\/?images\//,
-            "",
-          )}`;
+          const storagePath = `images/${decodedPath}`;
           const imageRef = storageRef(storage, storagePath);
           return deleteObject(imageRef);
         });
 
         await Promise.all(deletePromises);
-
-        setItems(items.filter((item) => item.id !== itemId));
-
         Swal.fire(alertList.successMessage("삭제가 완료되었습니다."));
       } catch (error) {
-        console.error("아이템 및 이미지 삭제 중 오류 발생:", error);
+        console.error("Error deleting item:", error);
         Swal.fire(alertList.errorMessage("삭제에 실패했습니다."));
       }
     }
   };
-
   return (
     <Container>
-      {items.map((item) => (
-        <ItemBox key={item.id} onClick={() => handleItemClick(item.id)}>
-          {isAdmin && (
-            <DeleteButton
-              onClick={(e) => {
-                e.stopPropagation();
-                deleteItemWithImage(item.id, item.itemDescription);
-              }}
-            />
-          )}
-          <ItemImage
-            src={
-              item.itemDescription && item.itemDescription.length > 0
-                ? item.itemDescription[0].imageUrl
-                : "https://bunny-pit-image.s3.ap-northeast-2.amazonaws.com/image.png"
-            }
-            alt={`Product ${item.id}`}
-          />
-          <ItemName>{item.name}</ItemName>
-          <ItemPrice>{item.price.toLocaleString()} 원</ItemPrice>
-        </ItemBox>
+      {data?.pages.map((page, i) => (
+        <React.Fragment key={i}>
+          {page.items.map((item: Item) => (
+            <ItemBox key={item.id} onClick={() => handleItemClick(item.id)}>
+              {isAdmin && (
+                <DeleteButton
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    deleteItemWithImage(item.id, item.itemDescription);
+                  }}
+                />
+              )}
+              <ItemImage
+                src={
+                  item.itemDescription[0]?.imageUrl ||
+                  "https://bunny-pit-image.s3.ap-northeast-2.amazonaws.com/image.png"
+                }
+                alt={`Product ${item.id}`}
+              />
+              <ItemName>{item.name}</ItemName>
+              <ItemPrice>{item.price.toLocaleString()} 원</ItemPrice>
+            </ItemBox>
+          ))}
+        </React.Fragment>
       ))}
       <EndMessage ref={ref}>
-        {hasMore ? "제품 정보를 불러오는 중입니다." : ""}
+        {isFetchingNextPage ? "제품 정보를 불러오는 중입니다." : ""}
       </EndMessage>
     </Container>
   );
